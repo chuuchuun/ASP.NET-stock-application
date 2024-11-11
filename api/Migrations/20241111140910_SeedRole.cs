d74e34167d100cee2871cba6c56f516c63170fc7
chuuchuun <allakrylova136@gmail.com>
2024-11-11 15:13:27 +0100

    added users using JWT

---
+++ api/Migrations/20241111140910_SeedRole.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace api.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedRole : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { "27127768-a623-4657-8455-964827d20cc5", null, "User", "USER" },
+                    { "8f336d1d-6a39-4604-8c08-59aa208c3023", null, "Admin", "ADMIN" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "27127768-a623-4657-8455-964827d20cc5");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "8f336d1d-6a39-4604-8c08-59aa208c3023");
+        }
+    }
+}
---
+++ api/Repositories/StockRepository.cs
@@ -0,0 +1,92 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using api.Data;
+using api.Dtos.Stock;
+using api.Helpers;
+using api.Interfaces;
+using api.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace api.Repositories
+{
+    public class StockRepository : IStockRepository
+    {
+        private readonly ApplicationDBContext _context;
+        public StockRepository(ApplicationDBContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Stock> CreateAsync(Stock stockModel)
+        {
+            await _context.Stocks.AddAsync(stockModel);
+            await _context.SaveChangesAsync();
+            return stockModel;
+        }
+
+        public async Task<Stock?> DeleteAsync(int id)
+        {
+            var stock = await _context.Stocks.FirstOrDefaultAsync(s => s.Id == id);
+            if(stock == null){
+                return null;
+            }
+            _context.Stocks.Remove(stock);
+            await _context.SaveChangesAsync();
+
+            return stock;
+        }
+
+        public async Task<List<Stock>> GetAllAsync(QueryObject query)
+        {
+           var stocks = _context.Stocks.Include(c=> c.Comments).AsQueryable();
+            if(!string.IsNullOrWhiteSpace(query.CompanyName)){
+                stocks = stocks.Where(s => s.CompanyName.Contains(query.CompanyName));
+
+            }
+            if(!string.IsNullOrWhiteSpace(query.Symbol)){
+                stocks = stocks.Where(s => s.Symbol.Contains(query.Symbol));
+            }
+
+            if(!string.IsNullOrWhiteSpace(query.SortBy)){
+               if(query.SortBy.Equals("Symbol", StringComparison.OrdinalIgnoreCase)){
+                    stocks = query.IsDescending ? stocks.OrderByDescending(s => s.Symbol) : stocks.OrderBy(s => s.Symbol);
+               } 
+            }
+
+            var skipNumber = (query.PageNumber - 1) * query.PageSize;
+            return await stocks.Skip(skipNumber).Take(query.PageSize).ToListAsync();
+        }
+
+        public async Task<Stock?> GetByIdAsync(int id)
+        {
+            return await _context.Stocks.Include(c=> c.Comments).FirstOrDefaultAsync(i => i.Id == id);
+        }
+
+        public async Task<Stock?> UpdateAsync(int id, UpdateStockDto updateDto)
+        {
+            var stock = await _context.Stocks.FirstOrDefaultAsync(s => s.Id == id);
+
+            if(stock == null){
+                return null;
+            }   
+
+            stock.Symbol = updateDto.Symbol;
+            stock.Industry = updateDto.Industry;
+            stock.Purchase = updateDto.Purchase;
+            stock.MarketCap = updateDto.MarketCap;
+            stock.CompanyName = updateDto.CompanyName;
+
+            await _context.SaveChangesAsync(); 
+
+            return stock; 
+        }
+
+        public Task<bool> StockExists (int id){
+            return _context.Stocks.AnyAsync(s => s.Id == id);
+        }
+
+       
+    }
+}
